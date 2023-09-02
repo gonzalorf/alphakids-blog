63ea0d392eee2e344189e83e22dbe2019edddca0
Gonzalo <gonzalorf@gmail.com>
2023-09-02 18:40:28 -0300

    Current User Service.

---
+++ src/Backend/NanoBlogEngine.WebApi/Services/CurrentUserService.cs
@@ -0,0 +1,17 @@
+﻿using NanoBlogEngine.Application.Users.Services;
+using NanoBlogEngine.Domain.Users;
+using System.Security.Claims;
+
+namespace NanoBlogEngine.WebApi.Services;
+
+public class CurrentUserService : ICurrentUserService
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    UserId? ICurrentUserService.UserId => new UserId(Guid.Parse(_httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier)));
+}
